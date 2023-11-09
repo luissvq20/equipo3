dce1032ffe9b6f7c3f2088d5ccd51c10bbc0df70
luissvq20 <eleesehache3@gmail.com>
2023-11-09 13:11:29 +0100

    formfun1

---
+++ vende_Tucoche/js/funcionalidadform.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", init);
+function init() {
+    form1();
+       
+}
+
+function form1(){
+    document.getElementById("f1").style.display = "none";
+    document.getElementById("f2").style.display = "none";
+    document.getElementById("f3").style.display = "block";
+
+
+}
