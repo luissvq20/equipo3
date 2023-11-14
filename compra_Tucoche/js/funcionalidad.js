d27be9fc621904bf080ab63f3a61d5f650d5dae0
luissvq20 <eleesehache3@gmail.com>
2023-11-14 11:16:34 +0100

    compratucoche over

--- compra_Tucoche/js/funcionalidad.js
+++ compra_Tucoche/js/funcionalidad.js
@@ -12,7 +12,7 @@ function init() {
 
 function mostrarCoches(datos) {
     var contenedor_coches = document.getElementById("coches");
-    contenedor_coches.innerHTML=" ";
+    contenedor_coches.innerHTML = " ";
 
 
     datos.forEach((coche, index) => {
@@ -32,7 +32,7 @@ function mostrarCoches(datos) {
                     <p id="descripcion_1">${coche.descripcion_1}</p>
                     <p id="descripcion_2">${coche.descripcion_2}</p>
                 </div>
-                <input type="button" class="botones" value="Ver info">
+                <input type="button" class="botones" onclick=showmodal(${coche.id}) value="Ver info">
 
             </div>
         `;
@@ -48,28 +48,28 @@ function filtro() {
     var combustible_seleccionada = document.getElementById("combustible").value;
     var plazas_seleccionada = document.getElementById("plazas").value;
 
-    var coches_filtrados=coches_json
+    var coches_filtrados = coches_json
 
-   
 
-    if(marca_seleccionada){
-        coches_filtrados=coches_json.filter(coches=>coches.marca==marca_seleccionada);
+
+    if (marca_seleccionada) {
+        coches_filtrados = coches_json.filter(coches => coches.marca == marca_seleccionada);
     }
-    if(ano_seleccionada){
-        coches_filtrados=coches_filtrados.filter(coches=>coches.ano==ano_seleccionada);
+    if (ano_seleccionada) {
+        coches_filtrados = coches_filtrados.filter(coches => coches.ano == ano_seleccionada);
     }
-    if(precio_seleccionada){
-        coches_filtrados=coches_filtrados.filter(coches=>coches.precio==precio_seleccionada);
+    if (precio_seleccionada) {
+        coches_filtrados = coches_filtrados.filter(coches => coches.precio == precio_seleccionada);
     }
-    if(combustible_seleccionada){
-        coches_filtrados=coches_filtrados.filter(coches=>coches.combustible==combustible_seleccionada);
+    if (combustible_seleccionada) {
+        coches_filtrados = coches_filtrados.filter(coches => coches.combustible == combustible_seleccionada);
     }
-    if(plazas_seleccionada){
-        coches_filtrados=coches_filtrados.filter(coches=>coches.plazas==plazas_seleccionada);
+    if (plazas_seleccionada) {
+        coches_filtrados = coches_filtrados.filter(coches => coches.plazas == plazas_seleccionada);
     }
 
     var contenedor_coches = document.getElementById("coches");
-    contenedor_coches.innerHTML=" ";
+    contenedor_coches.innerHTML = " ";
     coches_filtrados.forEach((coche, index) => {
         var div_coche = document.createElement("div");
         div_coche.className = "col"
@@ -87,7 +87,7 @@ function filtro() {
                 <p id="descripcion_1">${coche.descripcion_1}</p>
                 <p id="descripcion_2">${coche.descripcion_2}</p>
                 </div>
-                <input type="button" class="botones" value="Ver info">
+                <input type="button" class="botones" onclick=showmodal(${coche.id}) value="Ver info">
 
             </div>
         `;
@@ -96,13 +96,144 @@ function filtro() {
 }
 
 function borrar_filtro() {
-    document.getElementById("marca").value ="";
-    document.getElementById("ano").value="";
-    document.getElementById("precio").value="";
-    document.getElementById("combustible").value="";
-    document.getElementById("plazas").value="";
+    document.getElementById("marca").value = "";
+    document.getElementById("ano").value = "";
+    document.getElementById("precio").value = "";
+    document.getElementById("combustible").value = "";
+    document.getElementById("plazas").value = "";
 
     mostrarCoches(coches_json);
 
-   
+
+}
+
+function showmodal(id) {
+
+    var coches_filtrados = coches_json
+    coche_modal = coches_json.filter(coches => coches.id == id);
+
+    var modal = document.getElementById("modal");
+    var nombre = document.getElementById("nombre");
+    modal.innerHTML = " ";
+
+    coche_modal.forEach((coche, index) => {
+        var div_cochemodal = document.createElement("div");
+
+
+        div_cochemodal.innerHTML = `                
+    <div class="">
+    <div class="divimagen">
+        <img src="/images/${coche.imagen_coche}" alt="" class="img-galeria">
+        <div class="texto">
+            <p class="desde">${coche.desde_precio}</p>
+            <p class="precio">${coche.precio}€</p>
+            <p class="nombre">${coche.nombre}</p>
+            <p>${coche.descripcion_1}</p>
+        </div>
+    </div>
+</div>
+
+<div class="btn-group">
+    <button type="button" class="btn btn-warning dropdown-toggle" data-bs-toggle="dropdown"
+        aria-expanded="false">
+        FICHA TECNICA
+    </button>
+    <ul class="dropdown-menu">
+        <li>
+            <p class="dropdown-item" href="#"><strong>Velocidad máxima 220 km/h
+                </strong><br>
+                <strong>Aceleración 0-100 km/h 8,7 s</strong> <br>
+                <strong>Consumo NEDC:</strong><br>
+                Extraurbano 3,6 l/100 km <br>
+                Urbano 5 l/100 km <br>
+                Medio 4,1 l/100 km
+            </p>
+        </li>
+    </ul>
+</div>
+
+<div class="btn-group">
+    <button type="button" class="btn btn-warning dropdown-toggle" data-bs-toggle="dropdown"
+        aria-expanded="false">
+        MULTIMEDIA
+    </button>
+    <ul class="dropdown-menu">
+        <li>
+            <p class="dropdown-item" href="#">
+            <p>
+                Ocho altavoces. <br>
+                Equipo de audio con radio FM. <br>
+                pantalla táctil. <br>
+                Control remoto de audio en el volante. <br>
+                Conexión para: entrada AUX delantera y USB delantero.
+            </p>
+        </li>
+    </ul>
+</div>
+
+<div class="btn-group">
+    <button type="button" class="btn btn-warning dropdown-toggle" data-bs-toggle="dropdown"
+        aria-expanded="false">
+        SEGURIDAD
+    </button>
+    <ul class="dropdown-menu">
+        <li>
+            <p class="dropdown-item" href="#">
+            <p>
+                Airbag lateral de cortina delantero y trasero.<br>
+                Airbag frontal del conductor, airbag frontal del acompañante
+                desconectable.<br>
+                Airbags laterales delanteros. <br>
+                Preparación Isofix. <br>
+                Airbag de rodilla para el conductor.</p>
+        </li>
+    </ul>
+</div>
+
+<div class="btn-group">
+    <button type="button" class="btn btn-warning dropdown-toggle" data-bs-toggle="dropdown"
+        aria-expanded="false">
+        INTERIOR
+    </button>
+    <ul class="dropdown-menu">
+        <li>
+            <p>
+                <strong>Acabados de lujo: </strong><br>
+                pomo de la palanca de cambios en aluminio y cuero. <br>
+                consola central en negro piano. <br>
+                Puertas en símil fibra carbono. <br>
+                Tablero en simil fibra de carbono.
+            </p>
+        </li>
+    </ul>
+</div>
+
+
+`;
+        modal.appendChild(div_cochemodal);
+
+        document.getElementById("exampleModal").className="modal face show"
+        var div_opacity = document.createElement("div");
+        div_opacity.className="modal-backdrop fade show";
+        
+        document.getElementsByTagName("body")[0].appendChild(div_opacity);
+        document.getElementById("exampleModal").style.display="block"
+
+    })
+}
+
+function closemodal(id) {
+    //ocultar el modal
+        document.getElementById("exampleModal").style.display="none";
+    
+    //restablecer fondo
+        var div_opacity = document.querySelector(".modal-backdrop.fade.show");
+        if (div_opacity) {
+            div_opacity.parentNode.removeChild(div_opacity);
+        }
+
+    //restaurar
+        document.getElementById("exampleModal").className = "modal face";
+
+
 }
--- vende_Tucoche/js/funcionalidadform.js
+++ vende_Tucoche/js/funcionalidadform.js
@@ -8,6 +8,17 @@ function form1(){
     document.getElementById("f1").style.display = "block";
     document.getElementById("f2").style.display = "none";
     document.getElementById("f3").style.display = "none";
-
-
 }
+
+function form2(){
+    document.getElementById("f1").style.display = "none";
+    document.getElementById("f2").style.display = "block";
+    document.getElementById("f3").style.display = "none";
+}
+
+function form3(){
+    document.getElementById("f1").style.display = "none";
+    document.getElementById("f2").style.display = "none";
+    document.getElementById("f3").style.display = "block";
+}
+
